95ad1ada3212e1676e7b7575f15e959835e52a43
Dawoodjaveed <54910044+Dawoodjaveed@users.noreply.github.com>
2021-04-23 23:53:06 +0500

    fix

--- CoffeeShop/settings.py
+++ CoffeeShop/settings.py
@@ -88,8 +88,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 
 

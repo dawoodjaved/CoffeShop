17ebb5fa00cf7047011ad5e2e47c02eaf4f93437
Dawoodjaveed <54910044+Dawoodjaveed@users.noreply.github.com>
2021-04-23 23:47:11 +0500

    procfilesd

--- CoffeeShop/settings.py
+++ CoffeeShop/settings.py
@@ -88,7 +88,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 

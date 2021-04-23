8f9951b8e46d01cf43915a107ec48947f434b7ba
Dawoodjaveed <54910044+Dawoodjaveed@users.noreply.github.com>
2021-04-23 23:41:56 +0500

    static

--- CoffeeShop/settings.py
+++ CoffeeShop/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
